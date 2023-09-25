5191802b96b295592ac27e53fb462e2633e591a9
Abel <48522882+keihn@users.noreply.github.com>
2023-09-25 21:23:23 +0100

    added footer

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+(function postClickDrag() {
+    const slider = document.querySelector('.footer-posts');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(walk);
+    });
+})()
