3485d733bf7962f8f063d70cfd0bf43aac4bf413
Abel <48522882+keihn@users.noreply.github.com>
2023-10-18 05:48:03 +0100

    footer posts fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
-(function postClickDrag() {
-    const slider = document.querySelector('.footer-posts');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
 
-    slider.addEventListener('click', (e) => {
-        isDown = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+// const slider = document.querySelector('.footer-posts');
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        console.log(e.pageX)
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 10; //scroll-fast
-        slider.scrollLeft = scrollLeft - walk;
-    });
-})()
+// slider.addEventListener('click', (e) => {
+//     isDown = true;
+//     startX = e.pageX - slider.offsetLeft;
+//     scrollLeft = slider.scrollLeft;
+//     console.log(e.pageX);
+// });
+
+// slider.addEventListener('mousemove', (e) => {
+//     if (!isDown) return;
+//     e.preventDefault();
+//     console.log(e.pageX)
+//     const x = e.pageX - slider.offsetLeft;
+//     const walk = (x - startX) * 10; //scroll-fast
+//     slider.scrollLeft = scrollLeft - walk;
+// });
 
 function copyToClip(){
     const text = document.getElementById('privacy-text').innerText;
